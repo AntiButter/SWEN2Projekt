6c5cffab6cface8a4025946ee25a979f132accc4
Simon Lercher <76847115+AntiButter@users.noreply.github.com>
2022-05-15 22:40:12 +0200

    ssd

---
+++ TourPlanner_Lercher_Polley/ViewModels/AddTourViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlanner.Models.Enum;
+
+namespace TourPlanner_Lercher_Polley.ViewModels
+{
+    internal class AddTourViewModel : ViewModelBase
+    {
+        public string TourName { get; set; }
+        public string TourTo { get; set; }
+        public string TourFrom { get; set; }
+        public string TourDescription { get; set; }
+        public TransportType TransportType { get; set; }
+
+        public void selectOption_changed()
+        {
+
+        }
+        
+
+    }
+}
