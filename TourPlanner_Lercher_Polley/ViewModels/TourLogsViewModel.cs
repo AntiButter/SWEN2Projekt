7fccb63fe74d0a5df40bc1746d8cfe340b9272aa
Simon Lercher <if20b117@technikum-wien.at>
2022-05-24 12:21:54 +0200

    Create TourLogsViewModel.cs

---
+++ TourPlanner_Lercher_Polley/ViewModels/TourLogsViewModel.cs
@@ -0,0 +1,13 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner_Lercher_Polley.ViewModels
+{
+    public class TourLogsViewModel : ViewModelBase
+    {
+        
+    }
+}
